3923664e3d255d9a0ba053401084c7f9b7b435aa
mastashake08 <jyrone.parker@gmail.com>
2020-11-08 18:51:52 -0500

    added billing portal

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return Inertia\Inertia::render('Dashboard');
 })->name('dashboard');
+
+Route::get('/billing-portal', function(Request $request) {
+    return $request->user()->redirectToBillingPortal();
+});
